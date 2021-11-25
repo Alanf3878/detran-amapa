bf3af8af29991d9f69319fe69ff141bda805c0c3
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-11-25 10:36:44 -0300

    update

---
+++ src/components/header/index.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import * as S from "./style"
+
+const Header = () => {
+  return (
+   <S.Header>
+     
+     
+             <S.Boxitem>
+                 <li>HOME</li>
+                 <li>REDE SOCIAIS</li>
+                 <li>SOBRE</li>
+                 <li>CONTATO</li>
+             </S.Boxitem>
+      
+     
+   </S.Header>
+  );
+};
+
+export default Header;
---
+++ src/components/header/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+background-color: #FFF;
+opacity: 0.9;
+  box-shadow: rgb(0 0 0 / 10%) 0px 3px 10px;
+width: 100%;
+height: 10vh;
+
+`
+export const Nav = styled.div`
+height: 100%;
+display: flex;
+
+`
+export const Boxitem = styled.ul`
+max-width: 1333px;
+margin: 0 auto;
+height: 100%;
+display: flex;
+justify-content: space-around;
+align-items: center;
+list-style: none;
+
+
+li{
+    color: #000;
+    
+    
+}
+`

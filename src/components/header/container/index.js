9231a7f37929813921751aa4a63a62d0796e5ce4
Alan Fernandes <81441721+Alanf3878@users.noreply.github.com>
2021-11-25 12:06:20 -0300

    update

---
+++ src/components/header/container/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import * as S from "./style"
+import { graphql, useStaticQuery } from 'gatsby';
+
+const Nav = () => {
+  return (
+  <div>
+
+  </div>
+  );
+};
+
+export default Nav;
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -11,21 +11,19 @@ height: 10vh;
 export const Nav = styled.div`
 height: 100%;
 display: flex;
-
 `
 export const Boxitem = styled.ul`
 max-width: 1333px;
 margin: 0 auto;
 height: 100%;
 display: flex;
-justify-content: space-around;
+justify-content: center;
 align-items: center;
 list-style: none;
 
 
 li{
     color: #000;
-    
-    
+    width: 10rem;
 }
 `
